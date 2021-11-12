ca984c3f21ebbc1e178ce4a02826f79ebe0a1327
rso2794 <rso2794@conestogac.on.ca>
2021-11-11 21:09:58 -0500

    init 1

--- MainProject/network.h
+++ MainProject/network.h
@@ -1,2 +1,15 @@
 #pragma once
 
+#include "module_interface.h"
+#include "iostream";
+#include "string";
+#include "vector";
+
+class Network : public module_interface
+{
+private:
+
+
+public:
+
+};
