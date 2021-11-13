f6d09782335586f39f3fc16801d9669f272bf733
rso2794 <rso2794@conestogac.on.ca>
2021-11-12 19:58:05 -0500

    network class v1

--- MainProject/network.h
+++ MainProject/network.h
@@ -9,7 +9,24 @@ class Network : public module_interface
 {
 private:
 
+	/*
+	Note:
+	average WIFI speed = 12 - 25 Mbps
+	average ethernet speed = 45 - 100MBps
+	*/
+	double maxBandwidth = 800; //units in Megabits(Mb)
+	double minBandwidth = 8; // 8Mb = 1MB
+	double currentMax = 80; // <= 10MBps
+	double currentSpeed;
+	vector <string> connectedDevices;
+
 
 public:
+	Network();
 
+	SimInfo getSim();
+
+	SensorInfo getSensorInfo();
+
+	void ChangeValue(string, string);
 };
